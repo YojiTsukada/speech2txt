ee0abd51c5981efb3c6dd72e735454393e8875a4
YojiTsukada <>
2017-11-23 23:44:35 +0900

    Update transcript.py

---
+++ transcript.py
@@ -0,0 +1,38 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname("__file__"),
+    'data',
+    'english_mono.flac')
+
+print(file_name)
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz=44100,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+alternatives = response.results[0].alternatives
+
+count = len(response.results)
+
+for alternative in alternatives:
+    print('Transcript: {}'.format(alternative.transcript))
+    print('Confidence: {}'.format(alternative.confidence))
